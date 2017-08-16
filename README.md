6cb226ab07446bd2406fde7e48ecb563b741cfe0
Brent Vollebregt <volleys2468@gmail.com>
2017-08-16 18:19:30 +1200

    Beginning fundamentals

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # quick-script
 Run small scripts from a simple UI
+
+## Currently in Development
---
+++ quick-launch.py
@@ -0,0 +1,122 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+import pkgutil
+import importlib
+import json
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self, module_storage):
+        self.module_storage = module_storage
+        QtWidgets.QMainWindow.__init__(self)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setFixedSize(390, 450)
+        self.setStyleSheet("QWidget {color: #b1b1b1; background-color: #323232; border: 0px;}")
+        self.centralwidget = QtWidgets.QWidget(self)
+
+        # Search bar setup
+        self.searchBar = QtWidgets.QLineEdit(self.centralwidget)
+        self.searchBar.setGeometry(QtCore.QRect(5, 5, 281, 40))
+        self.searchBar.setStyleSheet("background: transparent; padding-left: 8px; color: white; font: 15pt; border: 1px solid #ffffff;")
+        self.searchBar.setPlaceholderText("Search")
+
+        # Close button setup
+        icon = QtGui.QPixmap('images/close.png')
+        icon = icon.scaled(40, 40, QtCore.Qt.KeepAspectRatio)
+        self.closeLabel = self.setup_static_button(QtCore.QRect(345, 5, 40, 40), icon)
+        self.closeLabel.mousePressEvent = self.closeButtonAction
+
+        # Settings button setup
+        icon = QtGui.QPixmap('images/settings.png')
+        icon = icon.scaled(30, 30, QtCore.Qt.KeepAspectRatio)
+        self.settingsLabel = self.setup_static_button(QtCore.QRect(295, 5, 40, 40), icon)
+        self.settingsLabel.mousePressEvent = self.settingsButtonAction
+
+        # Scroll area setup
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setGeometry(QtCore.QRect(0, 50, 391, 401))
+        self.scrollArea.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.scrollArea.setStyleSheet("""QScrollArea {border: 0px;}""")
+        self.form = QtWidgets.QFormLayout()
+
+        # Adding dynamic labels
+        files = [i for i in self.module_storage]
+        self.file_labels = {}
+        for file in files:
+            self.file_labels[file] = self.setup_dynamic_label(file, ("File: " + file))
+            self.form.addRow(self.file_labels[file])
+
+        # Grouping
+        self.groupbox = QtWidgets.QGroupBox()
+        self.groupbox.setLayout(self.form)
+        self.scrollArea.setWidget(self.groupbox)
+        self.setCentralWidget(self.centralwidget)
+
+    def setup_static_button(self, position, icon):
+        tmp_label = QtWidgets.QLabel(self.centralwidget)
+        tmp_label.setGeometry(position)
+        tmp_label.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        tmp_label.setStyleSheet("QLabel {border: 1px solid #ffffff; font: 11pt; color: white;} QLabel:hover {border: 2px solid #ffaa00; color: #ffaa00;}")
+        tmp_label.setPixmap(icon)
+        tmp_label.setAlignment(QtCore.Qt.AlignCenter)
+        return tmp_label
+
+    def setup_dynamic_label(self, text, tool_tip):
+        tmp_label = QtWidgets.QLabel()
+        tmp_label.setText(text)
+        tmp_label.setStyleSheet("""QLabel {border: 1px solid #ffffff; font: 10pt; color: white;} QLabel:hover {border: 2px solid #ffaa00; color: #ffaa00;}""")
+        tmp_label.setAlignment(QtCore.Qt.AlignCenter)
+        tmp_label.setWordWrap(True)
+        tmp_label.setTextInteractionFlags(QtCore.Qt.TextSelectableByMouse)
+        tmp_label.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        tmp_label.setToolTip(tool_tip)
+        tmp_label.setFixedHeight(40)
+        tmp_label.setFixedWidth(self.width() - 20)
+        tmp_label.mousePressEvent = self.labelClickEvent
+        return tmp_label
+
+    def closeButtonAction(self, event):
+        if event.button() == 1:
+            self.close()
+
+    def settingsButtonAction(self, event):
+        pass
+
+    def labelClickEvent(self, event):
+        if event.button() == 1:
+            widgets = self.groupbox.children()
+            for widget in widgets:
+                hasGeo = getattr(widget, "mapToGlobal", None)
+                if not callable(hasGeo):
+                    continue
+                if widget.mapToGlobal(event.pos()) == event.globalPos():
+                    break
+            name = widget.text()
+            self.module_storage[name].main()
+            if getSettings()["close_on_run"]:
+                self.close()
+
+
+def getSettings():
+    with open('settings.json') as data_file:
+        return json.load(data_file)
+
+def setSettings(data):
+    try:
+        with open('settings.json', 'w') as outfile:
+            json.dump(data, outfile, indent=4, sort_keys=True)
+        return True
+    except:
+        return False
+
+
+if __name__ == "__main__":
+    module_index = [name for _, name, _ in pkgutil.iter_modules(['scripts'])]
+    module_storage = {}
+    for module in module_index:
+        module_storage[module] = importlib.import_module("scripts." + module)
+
+    app = QtWidgets.QApplication(sys.argv)
+    window = Window(module_storage)
+    window.show()
+    sys.exit(app.exec_())
+
